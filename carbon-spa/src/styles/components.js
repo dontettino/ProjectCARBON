1a0a9fe7232c32aa75b34f3370374f180ac33d37
Michal Pekar <pekac45@gmail.com>
2019-06-14 20:28:12 +0100

    styled components be styling

--- carbon-spa/src/styles/components.js
+++ carbon-spa/src/styles/components.js
@@ -13,7 +13,7 @@ export const GlobalStyle = createGlobalStyle`
 		text-align: center;
 	}
 	:root {
-		--main-color: #3d1f68;
+		--main-color: rgb(123, 0, 188);
 		--secondary-color: #efefef;
 	}
 `;
@@ -22,25 +22,67 @@ export const Button = styled.button`
 	color: var(--main-color);
 	text-transform: uppercase;
 	font-family: "Dosis", sans-serif;
-	font-size: inherit;
+	font-size: 2rem;
 	background-color: transparent;
-	height: 7rem;
-	width: 7rem;
+	height: 10rem;
+	width: 10rem;
 	cursor: pointer;
 
 	border: 1px groove black;
-	border-radius: 50%;
+	/* border-radius: 50%; */
 	text-align: center;
 	text-decoration: none;
 
 	transition: 0.3s;
 
-	position: absolute;
-	transform: translate(-50%);
-	top: 50%;
+	background: linear-gradient(0deg, #000, #272727);
+	position: relative;
+	margin: calc(50vh - 5rem);
 
 	:hover {
 		color: var(--secondary-color);
 		background-color: ${props => (props.danger ? "#ff2323" : "var(--main-color)")};
+	}
+
+	:before,
+	:after {
+		content: "";
+		position: absolute;
+		left: -2px;
+		top: -2px;
+		background: linear-gradient(
+			45deg,
+			#fb0094,
+			#0000ff,
+			#00ff00,
+			#ffff00,
+			#ff0000,
+			#fb0094,
+			#0000ff,
+			#00ff00,
+			#ffff00,
+			#ff0000
+		);
+		background-size: 400%;
+		width: calc(100% + 4px);
+		height: calc(100% + 4px);
+		z-index: -1;
+		animation: steam 20s linear infinite;
+	}
+
+	@keyframes steam {
+		0% {
+			background-position: 0 0;
+		}
+		50% {
+			background-position: 400% 0;
+		}
+		100% {
+			background-position: 0 0;
+		}
+	}
+
+	:after {
+		filter: blur(50px);
 	}
 `;
